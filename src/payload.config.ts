c9054e5d7f3ddf017d658505e481bc25cc048647
Lalith Kishore <lalithk3991@gmail.com>
2022-11-29 19:00:18 +0530

    Initial commit

---
+++ src/payload.config.ts
@@ -0,0 +1,28 @@
+import { buildConfig } from 'payload/config';
+import path from 'path';
+import Categories from './collections/Categories';
+import Posts from './collections/Posts';
+import Tags from './collections/Tags';
+import Users from './collections/Users';
+
+export default buildConfig({
+  serverURL: process.env.PAYLOAD_PUBLIC_BASE_DNS,
+  rateLimit: {
+    trustProxy: true,
+  },
+  admin: {
+    user: Users.slug,
+  },
+  collections: [
+    Categories,
+    Posts,
+    Tags,
+    Users,
+  ],
+  typescript: {
+    outputFile: path.resolve(__dirname, 'payload-types.ts')
+  },
+  graphQL: {
+    schemaOutputFile: path.resolve(__dirname, 'generated-schema.graphql'),
+  },
+});
